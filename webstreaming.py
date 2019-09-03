282cf69b8dad6903458e92945bf27fe4f28cf1d1
bellyk4real <bellyk4real@gmail.com>
2019-09-03 10:24:51 +0100

    initialized flask app as well as video stream object

--- webstreaming.py
+++ webstreaming.py
@@ -11,3 +11,17 @@ import imutils
 import time
 import cv2
 
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful when multiple browsers/tabs
+# are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+ 
+# initialize a flask object
+app = Flask(__name__)
+ 
+# initialize the video stream and allow the camera sensor to
+# warmup
+#vs = VideoStream(usePiCamera=1).start()
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
