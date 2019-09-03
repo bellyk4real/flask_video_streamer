ad517787970ae1dbdb0921f1fd66ce2e9580404d
bellyk4real <bellyk4real@gmail.com>
2019-09-03 10:24:04 +0100

    initialized flask app as well as video stream object

---
+++ webstreaming.py
@@ -0,0 +1,13 @@
+# import the necessary packages
+from video_streamer.motion_detection import SingleMotionDetector
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
